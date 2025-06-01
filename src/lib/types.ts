9adcaa78ad2ac4b79b1362869006c9e4e8f6e779
Christopher Lansdown <chris@chrislansdown.com>
2025-05-31 22:58:46 -0400

    feat: add Config class for app configuration

---
+++ src/lib/types.ts
@@ -0,0 +1,23 @@
+export class Config {
+  historyWidth: number;
+  apiKey: string;
+  defaultModel: string;
+  availableModels: string[];
+  systemPrompt: string;
+  allowWebSearch: boolean;
+  webSearchMaxResults: number;
+  includePreviousMessagesAsContext: boolean;
+  searchEngine: string;
+
+  constructor() {
+    this.historyWidth = 400;
+    this.apiKey = '';
+    this.defaultModel = '';
+    this.availableModels = [];
+    this.systemPrompt = '';
+    this.allowWebSearch = false;
+    this.webSearchMaxResults = 5;
+    this.includePreviousMessagesAsContext = true;
+    this.searchEngine = 'google';
+  }
+}
