d06158326bbd8a28918c050fc5ba6f78a8de2b09
Christopher Lansdown <chris@chrislansdown.com>
2025-06-16 16:19:19 -0400

    initial version of the help for tools

---
+++ src/ToolsHelp.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+
+</script>
+
+
+<div class="main">
+
+    <h2>Standard Research</h2>
+
+    <h2>Web Results</h2>
+
+    <h2>System Prompts</h2>
+
+    <h2>Deep Research</h2>
+
+    <h2>Experimentation Options</h2>
+
+
+
+
+
+</div>
+
+
+
+<style>
+    .main {
+        padding: 1rem;
+    }
+
+</style>
